f5299cd9a7e020f59921a7302990fd35469b575a
PhongPPT <workingcompany911@gmail.com>
2025-06-17 13:44:58 +0700

    add more condition remove icon in the table

--- Hide.js
+++ Hide.js
@@ -10,9 +10,9 @@
 
   kintone.events.on(events, async function (event) {
     const record = event.record;
-    
+
     let AppID = kintone.app.getId();
-    
+
     let GETFIELD = await kintone.api("/k/v1/preview/app/form/fields", "GET", {
       app: AppID,
     });
@@ -36,6 +36,23 @@
       }
     }
 
+    // Remove icon in the table
+    let elements = document.querySelectorAll(".recordlist-edit-gaia");
+    elements.forEach((element) => {
+      element.remove();
+    });
+
+    const recordRows = document.querySelectorAll(".recordlist-row-gaia");
+    recordRows.forEach((row) => {
+      row.addEventListener(
+        "dblclick",
+        function (e) {
+          e.stopImmediatePropagation();
+          e.preventDefault();
+        },
+        true
+      );
+    });
     return event;
   });
 })();
---
+++ Query-kintone.js
@@ -0,0 +1,14 @@
+(function () {
+  "use strict";
+
+  const events = [
+    "app.record.index.show",
+  ];
+
+  kintone.events.on(events, async function (events) {
+    const record = events.record;
+    console.log("record", record)
+    const HearSpace = kintone.app.getHeaderSpaceElement()
+    console.log("HearSpace", HearSpace)
+  })
+});
